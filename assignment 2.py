5cbc7899dc8f2db8faf62dcf751b3ed3d462ca35
tariqul3 <103868814+tariqul3@users.noreply.github.com>
2022-04-17 16:24:10 +0600

    Create assignment 2.py

---
+++ assignment 2.py
@@ -0,0 +1,12 @@
+#calculate the sum of all the digits of N
+# i can't understand the question and also can not undastand your solution
+#please explane
+N = 3 ,12345, 31203 ,2123
+
+sum = sum(N)
+print(sum)
+
+
+num = 12345, 31203 ,2123
+print(len(str(num)))
+
